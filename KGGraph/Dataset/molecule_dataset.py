c23915cc7c025366a97d691f6851d0df6d2b1cc1
phuocchung123 <chungnvp197@gmail.com>
2024-04-03 17:44:05 +0700

    update

--- KGGraph/Dataset/molecule_dataset.py
+++ KGGraph/Dataset/molecule_dataset.py
@@ -156,6 +156,6 @@ class MoleculeDataset(InMemoryDataset):
             torch.save((data, slices), self.processed_paths[0])
             
 if __name__ == '__main__':
-    dataset = MoleculeDataset('./dataset/classification/bbbp/', dataset='bbbp', decompose_type='brics')
+    dataset = MoleculeDataset('./dataset/classification/bbbp/', dataset='bbbp', decompose_type='jin')
     print(dataset)
     print(dataset[0])

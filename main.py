da0baeceae5143f1f2a13ebe1ef802389fc5ef33
phuocchung123 <chungnvp197@gmail.com>
2024-04-05 00:21:09 +0700

    update

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def main():
                         help='how the node features across layers are combined. last, sum, max or concat')
     parser.add_argument('--gnn_type', type=str, default="gin",
                         help='gnn_type (gin, gcn)')
-    parser.add_argument('--decompose_type', type=str, default="smotif",
+    parser.add_argument('--decompose_type', type=str, default="motif",
                         help='decompose_type (brics, jin, motif, smotif) (default: motif).')
     parser.add_argument('--dataset', type=str, default = 'sider',
                         help='[bbbp, bace, sider, clintox, tox21, toxcast, esol, freesolv, lipophilicity]')

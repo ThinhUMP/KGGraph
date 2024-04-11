5ae2b17a82455657dbf84ff3023365f4949c6ca7
phuocchung123 <chungnvp197@gmail.com>
2024-04-11 14:11:22 +0700

    update

--- pretrain.py
+++ pretrain.py
@@ -103,7 +103,7 @@ def main():
                         help='which gpu to use if any (default: 0)')
     parser.add_argument('--batch_size', type=int, default=32,
                         help='input batch size for training (default: 32)')
-    parser.add_argument('--epochs', type=int, default=1,
+    parser.add_argument('--epochs', type=int, default=100,
                         help='number of epochs to train (default: 100)')
     parser.add_argument('--lr', type=float, default=0.001,
                         help='learning rate (default: 0.001)')
@@ -112,7 +112,7 @@ def main():
     parser.add_argument('--num_layer', type=int, default=5,
                         help='number of GNN message passing layers (default: 5).')
     parser.add_argument('--emb_dim', type=int, default=512,
-                        help='embedding dimensions (default: 300)')
+                        help='embedding dimensions (default: 512)')
     parser.add_argument('--dropout_ratio', type=float, default=0.5,
                         help='dropout ratio (default: 0.2)')
     parser.add_argument('--JK', type=str, default="last",
@@ -129,11 +129,11 @@ def main():
     args = parser.parse_args()
 
 
-    torch.manual_seed(0)
-    np.random.seed(0)
+    torch.manual_seed(42)
+    np.random.seed(42)
     device = torch.device("cuda:" + str(args.device)) if torch.cuda.is_available() else torch.device("cpu")
     if torch.cuda.is_available():
-        torch.cuda.manual_seed_all(0)
+        torch.cuda.manual_seed_all(42)
     print('device', device)
 
     dataset = MoleculeDataset(args.dataset, args.decompose_type)

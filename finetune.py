18fbe6fd3a2e6dc97e9aadf831f0cc12e0bc26f6
Van-Thinh To <tvthinhd19@gmail.com>
2024-04-11 14:08:51 +0700

    update

--- finetune.py
+++ finetune.py
@@ -22,13 +22,13 @@ def main():
                         help='which gpu to use if any (default: 0)')
     parser.add_argument('--batch_size', type=int, default=32,
                         help='input batch size for training (default: 32)')
-    parser.add_argument('--training_rounds', type=int, default=1,
+    parser.add_argument('--training_rounds', type=int, default=3,
                         help='number of rounds to train to get the average test auc (default: 5)')
-    parser.add_argument('--epochs', type=int, default=1,
+    parser.add_argument('--epochs', type=int, default=150,
                         help='number of epochs to train (default: 100)')
     parser.add_argument('--lr_feat', type=float, default=0.0001,
                         help='learning rate (default: 0.0005)')
-    parser.add_argument('--lr_pred', type=float, default=0.001,
+    parser.add_argument('--lr_pred', type=float, default=0.0001,
                         help='learning rate for the prediction layer (default: 0.001)')
     parser.add_argument('--decay', type=float, default=0.0,
                         help='weight decay (default: 0)')

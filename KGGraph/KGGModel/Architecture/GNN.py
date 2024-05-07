fe0d5cc72fc2128e841177896ae4cd72035fd9e1
VanThinh To <tvthinh.d19@ump.edu.vn>
2024-05-07 11:42:52 +0700

    update

--- KGGraph/KGGModel/Architecture/GNN.py
+++ KGGraph/KGGModel/Architecture/GNN.py
@@ -39,19 +39,19 @@ class GNN(torch.nn.Module):
         
         self.x_embedding1 = torch.nn.Embedding(num_atom_type, emb_dim)
         self.x_embedding2 = torch.nn.Embedding(num_chirality_tag, emb_dim)
-        self.x_embedding3 = torch.nn.Embedding(num_hybri_1, emb_dim)
-        self.x_embedding4 = torch.nn.Embedding(num_hybri_2, emb_dim)
-        self.x_embedding5 = torch.nn.Embedding(num_hybri_3, emb_dim)
-        self.x_embedding6 = torch.nn.Embedding(num_hybri_4, emb_dim)
-        self.x_embedding7 = torch.nn.Embedding(num_hybri_5, emb_dim)
+        # self.x_embedding3 = torch.nn.Embedding(num_hybri_1, emb_dim)
+        # self.x_embedding4 = torch.nn.Embedding(num_hybri_2, emb_dim)
+        # self.x_embedding5 = torch.nn.Embedding(num_hybri_3, emb_dim)
+        # self.x_embedding6 = torch.nn.Embedding(num_hybri_4, emb_dim)
+        # self.x_embedding7 = torch.nn.Embedding(num_hybri_5, emb_dim)
 
         torch.nn.init.xavier_uniform_(self.x_embedding1.weight.data)
         torch.nn.init.xavier_uniform_(self.x_embedding2.weight.data)
-        torch.nn.init.xavier_uniform_(self.x_embedding3.weight.data)
-        torch.nn.init.xavier_uniform_(self.x_embedding4.weight.data)
-        torch.nn.init.xavier_uniform_(self.x_embedding5.weight.data)
-        torch.nn.init.xavier_uniform_(self.x_embedding6.weight.data)
-        torch.nn.init.xavier_uniform_(self.x_embedding7.weight.data)
+        # torch.nn.init.xavier_uniform_(self.x_embedding3.weight.data)
+        # torch.nn.init.xavier_uniform_(self.x_embedding4.weight.data)
+        # torch.nn.init.xavier_uniform_(self.x_embedding5.weight.data)
+        # torch.nn.init.xavier_uniform_(self.x_embedding6.weight.data)
+        # torch.nn.init.xavier_uniform_(self.x_embedding7.weight.data)
         
         ###List of MLPs
         self.gnns = torch.nn.ModuleList()
@@ -80,7 +80,8 @@ class GNN(torch.nn.Module):
         else:
             raise ValueError("unmatched number of arguments.")
         # + self.x_embedding2(x[:,1])
-        x = self.x_embedding1(x[:,0]) + self.x_embedding2(x[:,1]) + self.x_embedding3(x[:,2]) + self.x_embedding4(x[:,3]) + self.x_embedding5(x[:,4]) + self.x_embedding6(x[:,5]) + self.x_embedding7(x[:,6])
+        x = self.x_embedding1(x[:,0]) + self.x_embedding2(x[:,1]) 
+        # + self.x_embedding3(x[:,2]) + self.x_embedding4(x[:,3]) + self.x_embedding5(x[:,4]) + self.x_embedding6(x[:,5]) + self.x_embedding7(x[:,6])
 
         h_list = [x]
         

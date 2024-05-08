cf617cc0d75d4bada738823dc67f15355b682612
VanThinh To <tvthinh.d19@ump.edu.vn>
2024-05-08 23:56:59 +0700

    test unittest

---
+++ Test/KGGDecode/test_decoder.py
@@ -0,0 +1,12 @@
+import unittest
+
+class TestModelDecoder(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_forward_pass(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ Test/KGGProcessor/test_processor.py
@@ -0,0 +1,12 @@
+import unittest
+
+class TestProcessor(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_forward_pass(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()

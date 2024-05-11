4eca315c3d8a28889fd47f6d6e82c0f298f1900e
Van-Thinh To <tvthinhd19@gmail.com>
2024-05-11 10:33:19 +0700

    update

--- finetune.py
+++ finetune.py
@@ -92,7 +92,7 @@ def main():
     parser.add_argument(
         "--dataset",
         type=str,
-        default="bbbp",
+        default="sider",
         help="[bbbp, bace, sider, clintox, tox21, toxcast, esol, freesolv, lipophilicity]",
     )
     parser.add_argument(

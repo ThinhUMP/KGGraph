7579098a4e37f5e15ad5cf5b0c996d59a9c88fa9
Van-Thinh To <tvthinh.d19@ump.edu.vn>
2024-05-21 00:25:47 +0700

    update

--- finetune.py
+++ finetune.py
@@ -72,7 +72,7 @@ def main():
         "--emb_dim", type=int, default=512, help="embedding dimensions (default: 512)"
     )
     parser.add_argument(
-        "--dropout_ratio", type=float, default=0.7, help="dropout ratio (default: 0.5)"
+        "--dropout_ratio", type=float, default=0.6, help="dropout ratio (default: 0.5)"
     )
     parser.add_argument(
         "--JK",

dc9968a9451d9af5d340408768034d170a81ede9
Van-Thinh To <tvthinh.d19@ump.edu.vn>
2024-05-29 21:20:03 +0700

    update

--- pretrain.py
+++ pretrain.py
@@ -81,7 +81,7 @@ def main():
     parser.add_argument(
         "--output_model_file",
         type=str,
-        default="./saved_model_mlp_ce80_1layer/pretrain.pth",
+        default="./saved_model_mlp_ce_charger/pretrain.pth",
         help="filename to output the pre-trained model",
     )
     parser.add_argument(
@@ -161,9 +161,9 @@ def main():
         gnn_type=args.gnn_type,
     ).to(device)
 
-    if not os.path.isdir("./saved_model_mlp_ce80_1layer"):
-        os.mkdir("./saved_model_mlp_ce80_1layer")
-    if "pretrain.pth" in os.listdir("saved_model_mlp_ce80_1layer"):
+    if not os.path.isdir("./saved_model_mlp_ce_charger"):
+        os.mkdir("./saved_model_mlp_ce_charger")
+    if "pretrain.pth" in os.listdir("saved_model_mlp_ce_charger"):
         print("Continue pretraining")
         model.load_state_dict(torch.load(args.output_model_file))
 

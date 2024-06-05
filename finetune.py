3261f73892353b2b2cafd106d1b0ad0f98d7e0c6
Van-Thinh To <tvthinh.d19@ump.edu.vn>
2024-06-05 18:37:43 +0700

    update

--- finetune.py
+++ finetune.py
@@ -49,7 +49,7 @@ def main():
         help="number of epochs to train (default: 100)",
     )
     parser.add_argument(
-        "--lr_feat", type=float, default=0.001, help="learning rate (default: 0.0005)"
+        "--lr_feat", type=float, default=0.0005, help="learning rate (default: 0.0005)"
     )
     parser.add_argument(
         "--lr_pred",
@@ -58,7 +58,7 @@ def main():
         help="learning rate for the prediction layer (default: 0.001)",
     )
     parser.add_argument(
-        "--decay", type=float, default=0, help="weight decay (default: 0)"
+        "--decay", type=float, default=1e-4, help="weight decay (default: 0)"
     )
     parser.add_argument(
         "--num_layer",
@@ -70,7 +70,7 @@ def main():
         "--emb_dim", type=int, default=512, help="embedding dimensions (default: 512)"
     )
     parser.add_argument(
-        "--dropout_ratio", type=float, default=0.8, help="dropout ratio (default: 0.5)"
+        "--dropout_ratio", type=float, default=0.7, help="dropout ratio (default: 0.5)"
     )
     parser.add_argument(
         "--JK",
@@ -88,13 +88,13 @@ def main():
     parser.add_argument(
         "--dataset",
         type=str,
-        default="toxcast",
+        default="sider",
         help="[bbbp, bace, sider, clintox, tox21, toxcast, esol, freesolv, lipophilicity]",
     )
     parser.add_argument(
         "--input_model_file",
         type=str,
-        default="saved_model_mlp_ce_mask10_100/pretrain.pth",
+        default="saved_model_mlp_ce_dropout/pretrain.pth",
         help="filename to read the model (if there is any)",
     )
     parser.add_argument(

c06ee11239b2489bcc13258c8e28cdcb6c73280c
ChungNguyen <chungnvp197@gmail.com>
2024-06-06 18:47:11 +0700

    update

--- pretrain.py
+++ pretrain.py
@@ -37,7 +37,7 @@ def main():
     parser.add_argument(
         "--epochs",
         type=int,
-        default=60,
+        default=24,
         help="number of epochs to train (default: 100)",
     )
 

f5a4bc5e62574d97cb9fd71641e23709023b40b7
Van-Thinh To <tvthinh.d19@ump.edu.vn>
2024-06-07 19:26:09 +0700

    break ring and non-ring atoms

--- pretrain.py
+++ pretrain.py
@@ -81,7 +81,7 @@ def main():
     parser.add_argument(
         "--output_model_file",
         type=str,
-        default="./saved_model_mlp_ce60_1layer_x/pretrain.pth",
+        default="./saved_model_mlp_ce60_breaking/pretrain.pth",
         help="filename to output the pre-trained model",
     )
     parser.add_argument(
@@ -163,9 +163,9 @@ def main():
         gnn_type=args.gnn_type,
     ).to(device)
 
-    if not os.path.isdir("./saved_model_mlp_ce60_1layer_x"):
-        os.mkdir("./saved_model_mlp_ce60_1layer_x")
-    if "pretrain.pth" in os.listdir("saved_model_mlp_ce60_1layer_x"):
+    if not os.path.isdir("./saved_model_mlp_ce60_breaking"):
+        os.mkdir("./saved_model_mlp_ce60_breaking")
+    if "pretrain.pth" in os.listdir("saved_model_mlp_ce60_breaking"):
         print("Continue pretraining")
         model.load_state_dict(torch.load(args.output_model_file))
 

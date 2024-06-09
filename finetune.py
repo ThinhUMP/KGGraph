60fa6a36f51c9f46292d96b0f06f9234b7156144
Van-Thinh To <tvthinh.d19@ump.edu.vn>
2024-06-09 21:40:26 +0700

    update

--- finetune.py
+++ finetune.py
@@ -49,7 +49,7 @@ def main():
         help="number of epochs to train (default: 100)",
     )
     parser.add_argument(
-        "--lr_feat", type=float, default=0.001, help="learning rate (default: 0.0005)"
+        "--lr_feat", type=float, default=0.0005, help="learning rate (default: 0.0005)"
     )
     parser.add_argument(
         "--lr_pred",
@@ -58,7 +58,7 @@ def main():
         help="learning rate for the prediction layer (default: 0.001)",
     )
     parser.add_argument(
-        "--decay", type=float, default=0, help="weight decay (default: 0)"
+        "--decay", type=float, default=1e-3, help="weight decay (default: 0)"
     )
     parser.add_argument(
         "--num_layer",
@@ -82,19 +82,19 @@ def main():
     parser.add_argument(
         "--decompose_type",
         type=str,
-        default="tmotif",
+        default="motif",
         help="decompose_type (brics, jin, motif, smotif, tmotif) (default: motif).",
     )
     parser.add_argument(
         "--dataset",
         type=str,
-        default="clintox",
+        default="sider",
         help="[bbbp, bace, sider, clintox, tox21, toxcast, hiv, muv, esol, freesolv, lipo, qm7, qm8, qm9]",
     )
     parser.add_argument(
         "--input_model_file",
         type=str,
-        default="saved_model_mlp_ce60_tmotif/pretrain.pth",
+        default="saved_model_mlp_ce60_1layer_edge/pretrain.pth",
         help="filename to read the model (if there is any)",
     )
     parser.add_argument(

8f585b1b076182991ee3df22655cb1de7944b63f
Van-Thinh To <tvthinh.d19@ump.edu.vn>
2024-06-10 12:27:55 +0700

    update

--- finetune.py
+++ finetune.py
@@ -70,7 +70,7 @@ def main():
         "--emb_dim", type=int, default=512, help="embedding dimensions (default: 512)"
     )
     parser.add_argument(
-        "--dropout_ratio", type=float, default=0.7, help="dropout ratio (default: 0.5)"
+        "--dropout_ratio", type=float, default=0.6, help="dropout ratio (default: 0.5)"
     )
     parser.add_argument(
         "--JK",
@@ -88,13 +88,13 @@ def main():
     parser.add_argument(
         "--dataset",
         type=str,
-        default="clintox",
+        default="bace",
         help="[bbbp, bace, sider, clintox, tox21, toxcast, hiv, muv, esol, freesolv, lipo, qm7, qm8, qm9, ecoli]",
     )
     parser.add_argument(
         "--input_model_file",
         type=str,
-        default="saved_model_mlp_ce60_1layer_x/pretrain.pth",
+        default="saved_model_mlp_ce60_1layer_edge/pretrain.pth",
         help="filename to read the model (if there is any)",
     )
     parser.add_argument(

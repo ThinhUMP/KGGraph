03ffdb00639959d09bf7543990a78f5b1e38b42d
Van-Thinh To <tvthinh.d19@ump.edu.vn>
2024-06-18 13:39:41 +0700

    finetune

--- finetune.py
+++ finetune.py
@@ -51,7 +51,7 @@ def main():
         help="number of epochs to train (default: 100)",
     )
     parser.add_argument(
-        "--lr_feat", type=float, default=0.001, help="learning rate (default: 0.0005)"
+        "--lr_feat", type=float, default=0.0005, help="learning rate (default: 0.0005)"
     )
     parser.add_argument(
         "--lr_pred",
@@ -90,13 +90,13 @@ def main():
     parser.add_argument(
         "--dataset",
         type=str,
-        default="qm7",
+        default="sider",
         help="[bbbp, bace, sider, clintox, tox21, toxcast, hiv, muv, esol, freesolv, lipo, qm7, qm8, qm9]",
     )
     parser.add_argument(
         "--input_model_file",
         type=str,
-        default="saved_model_mlp_ce60_1layer/pretrain.pth",
+        default="saved_model_five_ce60/pretrain.pth",
         help="filename to read the model (if there is any)",
     )
     parser.add_argument(

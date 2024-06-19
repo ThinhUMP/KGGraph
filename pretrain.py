622adf171dcbf01726ef229887fe66e98c335465
Van-Thinh To <tvthinh.d19@ump.edu.vn>
2024-06-19 16:52:47 +0700

    woHyBo

--- pretrain.py
+++ pretrain.py
@@ -90,7 +90,7 @@ def main():
     parser.add_argument(
         "--output_model_file",
         type=str,
-        default="./saved_model_KGG-HyBowoPre/pretrain.pth",
+        default="./saved_model_KGGwoHyBo/pretrain.pth",
         help="filename to output the pre-trained model",
     )
     parser.add_argument(
@@ -176,9 +176,9 @@ def main():
         gnn_type=args.gnn_type,
     ).to(device)
 
-    if not os.path.isdir("./saved_model_KGG-HyBowoPre"):
-        os.mkdir("./saved_model_KGG-HyBowoPre")
-    if "pretrain.pth" in os.listdir("saved_model_KGG-HyBowoPre"):
+    if not os.path.isdir("./saved_model_KGGwoHyBo"):
+        os.mkdir("./saved_model_KGGwoHyBo")
+    if "pretrain.pth" in os.listdir("saved_model_KGGwoHyBo"):
         print("Continue pretraining")
         model.load_state_dict(torch.load(args.output_model_file))
 

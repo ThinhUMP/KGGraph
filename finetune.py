cab6befcd8eb545014ce0e1db2f30178e192902f
Van-Thinh To <tvthinh.d19@ump.edu.vn>
2024-06-21 02:23:18 +0700

    update

--- finetune.py
+++ finetune.py
@@ -41,7 +41,7 @@ def main():
     parser.add_argument(
         "--training_rounds",
         type=int,
-        default=4,
+        default=3,
         help="number of rounds to train to get the average test auc (default: 3)",
     )
     parser.add_argument(
@@ -72,8 +72,7 @@ def main():
         "--emb_dim", type=int, default=512, help="embedding dimensions (default: 512)"
     )
     parser.add_argument(
-        "--dropout_ratio", type=float, default=0.7, help="dropout ratio (default: 0.5)"
-        "--dropout_ratio", type=float, default=0.7, help="dropout ratio (default: 0.5)"
+        "--dropout_ratio", type=float, default=0.6, help="dropout ratio (default: 0.5)"
     )
     parser.add_argument(
         "--JK",
@@ -91,19 +90,19 @@ def main():
     parser.add_argument(
         "--dataset",
         type=str,
-        default="muv",
+        default="bace",
         help="[bbbp, bace, sider, clintox, tox21, toxcast, hiv, muv, esol, freesolv, lipo, qm7, qm8, qm9]",
     )
     parser.add_argument(
         "--input_model_file",
         type=str,
-        default="saved_model_KGGwoBondtype/pretrain.pth",
+        default="saved_model_mlp_ce60/pretrain.pth",
         help="filename to read the model (if there is any)",
     )
     parser.add_argument(
         "--seed",
         type=int,
-        default=42,
+        default=[42, 35, 102],
         help="Seed for splitting the dataset, minibatch selection, random initialization.",
     )
     parser.add_argument(
@@ -165,12 +164,12 @@ def main():
     for i in range(1, args.training_rounds + 1):
         print("====Round ", i)
         # set up seeds
-        seed_everything(args.seed)
+        seed_everything(args.seed[i-1])
 
         # dropout=[0.5,0.5,0.5,0.5]
         # decay=[1e-7,1e-6,1e-5,1e-4]
-        dropout=[0.5,0.6,0.7,0.8]
-        args.dropout_ratio = dropout[i-1]
+        # dropout=[0.5,0.6,0.7,0.8]
+        # args.dropout_ratio = dropout[i-1]
         # args.decay = decay[i-1]
         # args.decay = decay[i-1]
         # dataset_name=["bbbp", "sider", "clintox", "tox21", "toxcast", "hiv", "muv", "esol", "freesolv", "lipo", "qm7", "qm8", "qm9"]

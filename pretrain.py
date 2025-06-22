d264808e61375c35eeea91fc981b1dbb6323592d
Medicine-Artificial-Intelligence <medaiump@gmail.com>
2025-06-22 22:21:08 +0700

    update from lab 4

--- pretrain.py
+++ pretrain.py
@@ -49,7 +49,7 @@ def main():
     parser.add_argument(
         "--epochs",
         type=int,
-        default=47,
+        default=7,
         help="number of epochs to train (default: 60)",
     )
 

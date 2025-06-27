9c79d8f295068aa747ee6e283a636398182a5bba
tuyetminhphan <tuyetminh.work@gmail.com>
2025-06-27 17:44:40 +0700

    update from lab5

--- finetune.py
+++ finetune.py
@@ -33,7 +33,7 @@ def main():
         description="PyTorch implementation of training of graph neural networks"
     )
     parser.add_argument(
-        "--device", type=int, default=1, help="which gpu to use if any (default: 0)"
+        "--device", type=int, default=0, help="which gpu to use if any (default: 0)"
     )
     parser.add_argument(
         "--batch_size",
@@ -54,7 +54,7 @@ def main():
         help="number of epochs to train (default: 100)",
     )
     parser.add_argument(
-        "--lr_feat", type=float, default=0.0005, help="learning rate (default: 0.0005)"
+        "--lr_feat", type=float, default=0.001, help="learning rate (default: 0.0005)"
     )
     parser.add_argument(
         "--lr_pred",
@@ -63,7 +63,7 @@ def main():
         help="learning rate for the prediction layer (default: 0.001)",
     )
     parser.add_argument(
-        "--decay", type=float, default=2e-4, help="weight decay (default: 0)"
+        "--decay", type=float, default=0, help="weight decay (default: 0)"
     )
     parser.add_argument(
         "--num_layer",
@@ -98,7 +98,7 @@ def main():
     parser.add_argument(
         "--dataset",
         type=str,
-        default="sider",
+        default="clintox",
         help="[bbbp, bace, sider, clintox, tox21, toxcast, hiv, muv, esol, freesolv, lipo, qm7, qm8, qm9]",
     )
     parser.add_argument(
